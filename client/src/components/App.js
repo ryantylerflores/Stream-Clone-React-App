31519743bb85e7a88b61bae78aefdaa56f199649
Ryan Flores <rtflores90@gmail.com>
2019-01-09 10:30:13 -0800

    client

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom'; 
+
+import StreamList from '../components/streams/StreamList';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamShow from '../components/streams/StreamShow';
+import Header from './Header';
+import history from '../history';
+
+const App = () => {
+  return(
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
+          <Route path="/streams/delete/:id" component={StreamDelete} />
+          <Route path="/streams/show/:id" component={StreamShow} />
+        </div>
+      </Router>
+    </div>
+  ) 
+}
+
+export default App
